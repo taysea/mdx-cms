f1db728bc4798df9fb36610f5bf0c3b3f62521e6
Taylor Seamans <taylor.seamans@yahoo.com>
2020-02-13 13:00:26 -0800

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, grommet, Grommet, Heading } from 'grommet';
+
+const Index = () => {
+  return (
+    <Grommet theme={grommet} full>
+      <Box>
+        <Heading>Hello!</Heading>
+      </Box>
+    </Grommet>
+  );
+};
+
+export default Index;
